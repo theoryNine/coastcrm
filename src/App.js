8a760a32b611daecd38bccf5cbe9b62a5ef9280d
Ansel Colvin <anselcolvin@gmail.com>
2018-04-21 01:14:47 -0400

    Trying out styled components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './assets/logo.svg';
 import './App/App.css';
+import Nav from './App/Layout/Header/Nav.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+        <Nav />
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to CoastCRM</h1>
         </header>
---
+++ src/App/Layout/Header/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Background = styled.div`
+    background-color: white;
+    color: black;
+    height: 100px;
+    width: 100%;
+`;
+
+const Nav = () => (
+    <Background>Hi!</Background>
+  )
+
+export default Nav;
