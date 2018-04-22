d9b2be962136e8f3e073eff035aaa0eadec994e6
Ansel Colvin <anselcolvin@gmail.com>
2018-04-22 00:46:44 -0400

    Remove extra div name

--- src/App/Layout/Header/Nav.js
+++ src/App/Layout/Header/Nav.js
@@ -5,9 +5,11 @@ import Logo from '../../../components/Logo.js'
 const Background = styled.div`
     align-items: center;
     background-color: ${props => props.theme.coastSecondary};
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.55);
     color: ${props => props.theme.coastPrimary};
     display: flex;
     height: 100px;
+    opacity: .85;
     width: 100%;
 `
 
@@ -20,7 +22,7 @@ const InnerBg = styled.div`
 const Nav = () => (
     <nav className="nav" aria-label="Main navigation">
         <Background>
-            <InnerBg div className="container">
+            <InnerBg className="container">
                 <Logo />
                 <span>Text</span>
             </InnerBg>
