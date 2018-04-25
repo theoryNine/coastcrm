dc32d2d037bf977895110eb1d5401e665d844ac9
Ansel Colvin <anselcolvin@gmail.com>
2018-04-24 23:57:48 -0400

    Fix React error in footeR

--- src/App/Layout/Footer.js
+++ src/App/Layout/Footer.js
@@ -4,9 +4,9 @@ import './Footer/footer.css'
 class Footer extends React.Component {
     render() {
         return(
-            <footer class="footer">
-                <div class="container">
-                    <div class="content has-text-centered">
+            <footer className="footer">
+                <div className="container">
+                    <div className="content has-text-centered">
                     <p>
                         Copyright © 2018, Coast CRM. All rights reserved.
                     </p>
